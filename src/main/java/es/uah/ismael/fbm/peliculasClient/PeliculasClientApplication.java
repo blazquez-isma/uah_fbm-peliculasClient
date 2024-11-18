ea1742f060ddb7a3d9375f74194cfbe0c499d734
Ismael Blazquez Carabias <blazquez.isma.96@gmail.com>
2024-11-18 21:36:56 +0100

    Creacion del proyecto

---
+++ src/main/java/es/uah/ismael/fbm/peliculasClient/PeliculasClientApplication.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.peliculasClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uah/ismael/fbm/peliculasClient/PeliculasClientApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uah.ismael.fbm.peliculasClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
